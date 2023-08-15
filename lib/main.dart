a75d02585d77ef83aff08b353937c02b86b02f45
Misfit <misfit@Box.local>
2023-08-15 09:38:09 +0600

    - Configure DevicePreview for test ui responsiveness.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:my_flutter_template/features/home/home_page.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
